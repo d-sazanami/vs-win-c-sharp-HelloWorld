670b17725a65f2b39ec7d00a73abf819ffb5c742
Sazanami Hatsuyuki <dstrsazanami@gmail.com>
2023-12-31 14:25:12 +0900

    プロジェクト ファイルを追加します。

---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    /*
+     * HelloWorldチュートリアル
+     * https://learn.microsoft.com/ja-jp/visualstudio/ide/create-a-visual-basic-winform-in-visual-studio?view=vs-2022
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+        }
+    }
+}
